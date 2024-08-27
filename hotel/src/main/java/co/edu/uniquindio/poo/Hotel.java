05fd35d45bf547811c2a61ebd21e6567423868dd
LawrncD <159305192+LawrncD@users.noreply.github.com>
2024-08-26 20:18:30 -0500

    Creacion de la clase hotel

---
+++ hotel/src/main/java/co/edu/uniquindio/poo/Hotel.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.poo;
+
+public class Hotel {
+    private String nombre;
+    
+    public Hotel(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
